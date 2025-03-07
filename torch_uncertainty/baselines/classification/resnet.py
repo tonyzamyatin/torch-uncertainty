8d8500c37cd21153e9dc82be7098f7e2104a96b9
Anton <anton.zamyatin@student.tuwien.ac.at>
2025-03-07 19:27:17 +0000

    :books: Move ResNet baseline docstring from init() method to class definition

--- torch_uncertainty/baselines/classification/resnet.py
+++ torch_uncertainty/baselines/classification/resnet.py
@@ -26,6 +26,105 @@ ENSEMBLE_METHODS = [
 
 
 class ResNetBaseline(ClassificationRoutine):
+    r"""ResNet backbone baseline for classification providing support for
+    various versions and architectures.
+
+    Args:
+        num_classes (int): Number of classes to predict.
+        in_channels (int): Number of input channels.
+        loss (nn.Module): Training loss.
+        optim_recipe (Any): optimization recipe, corresponds to
+            what expect the `LightningModule.configure_optimizers()
+            <https://pytorch-lightning.readthedocs.io/en/stable/common/lightning_module.html#configure-optimizers>`_
+            method.
+        version (str):
+            Determines which ResNet version to use:
+
+            - ``"std"``: original ResNet
+            - ``"packed"``: Packed-Ensembles ResNet
+            - ``"batched"``: BatchEnsemble ResNet
+            - ``"masked"``: Masksemble ResNet
+            - ``"mimo"``: MIMO ResNet
+            - ``"mc-dropout"``: Monte-Carlo Dropout ResNet
+
+        arch (int):
+            Determines which ResNet architecture to use:
+
+            - ``18``: ResNet-18
+            - ``32``: ResNet-32
+            - ``50``: ResNet-50
+            - ``101``: ResNet-101
+            - ``152``: ResNet-152
+
+        style (str, optional): Which ResNet style to use. Defaults to
+        ``imagenet``.
+        normalization_layer (type[nn.Module], optional): Normalization layer
+            to use. Defaults to ``nn.BatchNorm2d``.
+        num_estimators (int, optional): Number of estimators in the ensemble.
+            Only used if :attr:`version` is either ``"packed"``, ``"batched"``,
+            ``"masked"`` or ``"mc-dropout"`` Defaults to ``None``.
+        dropout_rate (float, optional): Dropout rate. Defaults to ``0.0``.
+        mixup_params (dict, optional): Mixup parameters. Can include mixtype,
+            mixmode, dist_sim, kernel_tau_max, kernel_tau_std,
+            mixup_alpha, and cutmix_alpha. If None, no augmentations.
+            Defaults to ``None``.
+        width_multiplier (float, optional): Expansion factor affecting the width
+            of the estimators. Defaults to ``1.0``
+        groups (int, optional): Number of groups in convolutions. Defaults
+            to ``1``.
+        scale (float, optional): Expansion factor affecting the width of
+            the estimators. Only used if :attr:`version` is ``"masked"``.
+            Defaults to ``None``.
+        last_layer_dropout (bool): whether to apply dropout to the last layer only.
+        groups (int, optional): Number of groups in convolutions. Defaults to
+            ``1``.
+        conv_bias (bool, optional): Whether to include bias in the convolutional
+            layers. Defaults to ``False``.
+        scale (float, optional): Expansion factor affecting the width of the
+            estimators. Only used if :attr:`version` is ``"masked"``. Defaults
+            to ``None``.
+        alpha (float, optional): Expansion factor affecting the width of the
+            estimators. Only used if :attr:`version` is ``"packed"``. Defaults
+            to ``None``.
+        gamma (int, optional): Number of groups within each estimator. Only
+            used if :attr:`version` is ``"packed"`` and scales with
+            :attr:`groups`. Defaults to ``1``.
+        rho (float, optional): Probability that all estimators share the same
+            input. Only used if :attr:`version` is ``"mimo"``. Defaults to
+            ``1``.
+        batch_repeat (int, optional): Number of times to repeat the batch. Only
+            used if :attr:`version` is ``"mimo"``. Defaults to ``1``.
+        ood_criterion (str, optional): OOD criterion. Defaults to ``"msp"``.
+            MSP is the maximum softmax probability, logit is the maximum
+            logit, entropy is the entropy of the mean prediction, mi is the
+            mutual information of the ensemble and vr is the variation ratio
+            of the ensemble.
+        log_plots (bool, optional): Indicates whether to log the plots or not.
+            Defaults to ``False``.
+        save_in_csv (bool, optional): Indicates whether to save the results in
+            a csv file or not. Defaults to ``False``.
+        calibration_set (Callable, optional): Calibration set. Defaults to
+            ``None``.
+        eval_ood (bool, optional): Indicates whether to evaluate the
+            OOD detection or not. Defaults to ``False``.
+        eval_shift (bool): Whether to evaluate on shifted data. Defaults to
+        ``False``.
+        eval_grouping_loss (bool, optional): Indicates whether to evaluate the
+            grouping loss or not. Defaults to ``False``.
+        num_calibration_bins (int, optional): Number of calibration bins.
+            Defaults to ``15``.
+        pretrained (bool, optional): Indicates whether to use the pretrained
+            weights or not. Only used if :attr:`version` is ``"packed"``.
+            Defaults to ``False``.
+
+    Raises:
+        ValueError: If :attr:`version` is not either ``"std"``,
+            ``"packed"``, ``"batched"``, ``"masked"`` or ``"mc-dropout"``.
+
+    Returns:
+        LightningModule: ResNet baseline ready for training and evaluation.
+    """
+
     versions = {
         "std": resnet,
         "packed": packed_resnet,
@@ -77,104 +176,6 @@ class ResNetBaseline(ClassificationRoutine):
         num_calibration_bins: int = 15,
         pretrained: bool = False,
     ) -> None:
-        r"""ResNet backbone baseline for classification providing support for
-        various versions and architectures.
-
-        Args:
-            num_classes (int): Number of classes to predict.
-            in_channels (int): Number of input channels.
-            loss (nn.Module): Training loss.
-            optim_recipe (Any): optimization recipe, corresponds to
-                what expect the `LightningModule.configure_optimizers()
-                <https://pytorch-lightning.readthedocs.io/en/stable/common/lightning_module.html#configure-optimizers>`_
-                method.
-            version (str):
-                Determines which ResNet version to use:
-
-                - ``"std"``: original ResNet
-                - ``"packed"``: Packed-Ensembles ResNet
-                - ``"batched"``: BatchEnsemble ResNet
-                - ``"masked"``: Masksemble ResNet
-                - ``"mimo"``: MIMO ResNet
-                - ``"mc-dropout"``: Monte-Carlo Dropout ResNet
-
-            arch (int):
-                Determines which ResNet architecture to use:
-
-                - ``18``: ResNet-18
-                - ``32``: ResNet-32
-                - ``50``: ResNet-50
-                - ``101``: ResNet-101
-                - ``152``: ResNet-152
-
-            style (str, optional): Which ResNet style to use. Defaults to
-            ``imagenet``.
-            normalization_layer (type[nn.Module], optional): Normalization layer
-                to use. Defaults to ``nn.BatchNorm2d``.
-            num_estimators (int, optional): Number of estimators in the ensemble.
-                Only used if :attr:`version` is either ``"packed"``, ``"batched"``,
-                ``"masked"`` or ``"mc-dropout"`` Defaults to ``None``.
-            dropout_rate (float, optional): Dropout rate. Defaults to ``0.0``.
-            mixup_params (dict, optional): Mixup parameters. Can include mixtype,
-                mixmode, dist_sim, kernel_tau_max, kernel_tau_std,
-                mixup_alpha, and cutmix_alpha. If None, no augmentations.
-                Defaults to ``None``.
-            width_multiplier (float, optional): Expansion factor affecting the width
-                of the estimators. Defaults to ``1.0``
-            groups (int, optional): Number of groups in convolutions. Defaults
-                to ``1``.
-            scale (float, optional): Expansion factor affecting the width of
-                the estimators. Only used if :attr:`version` is ``"masked"``.
-                Defaults to ``None``.
-            last_layer_dropout (bool): whether to apply dropout to the last layer only.
-            groups (int, optional): Number of groups in convolutions. Defaults to
-                ``1``.
-            conv_bias (bool, optional): Whether to include bias in the convolutional
-                layers. Defaults to ``False``.
-            scale (float, optional): Expansion factor affecting the width of the
-                estimators. Only used if :attr:`version` is ``"masked"``. Defaults
-                to ``None``.
-            alpha (float, optional): Expansion factor affecting the width of the
-                estimators. Only used if :attr:`version` is ``"packed"``. Defaults
-                to ``None``.
-            gamma (int, optional): Number of groups within each estimator. Only
-                used if :attr:`version` is ``"packed"`` and scales with
-                :attr:`groups`. Defaults to ``1``.
-            rho (float, optional): Probability that all estimators share the same
-                input. Only used if :attr:`version` is ``"mimo"``. Defaults to
-                ``1``.
-            batch_repeat (int, optional): Number of times to repeat the batch. Only
-                used if :attr:`version` is ``"mimo"``. Defaults to ``1``.
-            ood_criterion (str, optional): OOD criterion. Defaults to ``"msp"``.
-                MSP is the maximum softmax probability, logit is the maximum
-                logit, entropy is the entropy of the mean prediction, mi is the
-                mutual information of the ensemble and vr is the variation ratio
-                of the ensemble.
-            log_plots (bool, optional): Indicates whether to log the plots or not.
-                Defaults to ``False``.
-            save_in_csv (bool, optional): Indicates whether to save the results in
-                a csv file or not. Defaults to ``False``.
-            calibration_set (Callable, optional): Calibration set. Defaults to
-                ``None``.
-            eval_ood (bool, optional): Indicates whether to evaluate the
-                OOD detection or not. Defaults to ``False``.
-            eval_shift (bool): Whether to evaluate on shifted data. Defaults to
-            ``False``.
-            eval_grouping_loss (bool, optional): Indicates whether to evaluate the
-                grouping loss or not. Defaults to ``False``.
-            num_calibration_bins (int, optional): Number of calibration bins.
-                Defaults to ``15``.
-            pretrained (bool, optional): Indicates whether to use the pretrained
-                weights or not. Only used if :attr:`version` is ``"packed"``.
-                Defaults to ``False``.
-
-        Raises:
-            ValueError: If :attr:`version` is not either ``"std"``,
-                ``"packed"``, ``"batched"``, ``"masked"`` or ``"mc-dropout"``.
-
-        Returns:
-            LightningModule: ResNet baseline ready for training and evaluation.
-        """
         params = {
             "arch": arch,
             "conv_bias": conv_bias,
